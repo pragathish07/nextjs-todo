fb67d8e3bdf061827329b574c71288a0604acd4c
Pragathish <pragarajesh779jd@gmail.com>
2024-04-12 16:28:36 +0530

    initial commit

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className='flex justify-center items-center gap-5 py-3'>
       <p className='text-gray-400'>© 2024 Todo App</p>
-      <h3><a href='https://www.github.com/pragathish07/' target='_blank' style={{textDecoration:"underline"}}>
+      <h3><a href='https://www.github.com/pragathish07/' style={{textDecoration:"underline"}}>
           check out my github profile here!</a></h3>
     </div>
   )
